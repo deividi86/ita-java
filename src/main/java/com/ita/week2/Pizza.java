672e7d4dc06e4b19c6628b00005ee7ac45b6e5b5
Deividi Silva <deividi@gmail.com>
2016-03-01 06:52:21 -0300

    begin week2

---
+++ src/main/java/com/ita/week2/Pizza.java
@@ -0,0 +1,12 @@
+package com.ita.week2;
+
+import java.util.HashMap;
+
+/**
+ * Created by Deividi on 01/03/2016.
+ */
+public class Pizza {
+
+	private static HashMap<String, Integer> ingredient;
+
+}
---
+++ src/main/java/com/ita/week2/ShoppingCart.java
@@ -0,0 +1,8 @@
+package com.ita.week2;
+
+/**
+ * Created by Deividi on 01/03/2016.
+ */
+public class ShoppingCart {
+
+}
