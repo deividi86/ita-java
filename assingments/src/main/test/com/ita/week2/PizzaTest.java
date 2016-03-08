fea4993bdcc0f0ed97738fc9b56313dc9a7577fb
Deividi Silva <deividi@gmail.com>
2016-03-07 23:26:12 -0300

    week 3 start

---
+++ assingments/src/main/test/com/ita/week2/PizzaTest.java
@@ -0,0 +1,19 @@
+package com.ita.week2;
+
+import org.junit.Test;
+
+/**
+ * Created by Deividi on 07/03/2016.
+ */
+public class PizzaTest {
+
+	@Test
+	public void testAddIngredient() throws Exception {
+
+	}
+
+	@Test
+	public void testGetPrice() throws Exception {
+
+	}
+}
