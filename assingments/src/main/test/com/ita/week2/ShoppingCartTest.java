5373f68cfc6a2535b0f3c6962fd97126fa4e11ed
Deividi Silva <deividi.silva@fluig.com>
2016-03-09 10:47:26 -0300

    shopping cart test

---
+++ assingments/src/main/test/com/ita/week2/ShoppingCartTest.java
@@ -0,0 +1,21 @@
+package com.ita.week2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by deividi.silva on 09/03/2016.
+ */
+public class ShoppingCartTest {
+
+    @Test
+    public void testAddToCart() throws Exception {
+
+    }
+
+    @Test
+    public void testPizzaTotal() throws Exception {
+
+    }
+}
