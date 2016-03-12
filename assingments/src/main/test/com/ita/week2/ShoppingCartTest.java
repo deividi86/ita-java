8e1e8d83932c75fc3ab8b062fb7ab6b23c863e7f
Deividi Silva <deividi@gmail.com>
2016-03-11 22:07:35 -0300

    cart test

--- assingments/src/main/test/com/ita/week2/ShoppingCartTest.java
+++ assingments/src/main/test/com/ita/week2/ShoppingCartTest.java
@@ -1,17 +1,29 @@
 package com.ita.week2;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 /**
  * Created by deividi.silva on 09/03/2016.
  */
 public class ShoppingCartTest {
 
+    private Pizza pizza = new Pizza();
+
+    @Before
+    public void setup() {
+        pizza.addIngredient("provolone");
+    }
+
     @Test
     public void testAddToCart() throws Exception {
+        ShoppingCart cart = new ShoppingCart();
+        cart.addToCart(pizza);
 
+        assertThat(1, equalTo(cart.pizzas.size()));
     }
 
     @Test
