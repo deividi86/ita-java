85966cdddd1d8faf7de663f951a66964714a3d59
Deividi <Deividi>
2016-04-02 09:15:31 -0300

    reorganize project

--- assingments/src/main/java/com/ita/week6/factory/GameEngineFactory.java
+++ assingments/src/main/java/com/ita/week6/factory/GameEngineFactory.java
@@ -1,4 +1,4 @@
-package com.ita.week6;
+package com.ita.week6.factory;
 
 /**
  * Created by deividi.silva on 30/03/2016.
---
+++ assingments/src/main/java/com/ita/week6/factory/ShufflerFactory.java
@@ -0,0 +1,7 @@
+package com.ita.week6.factory;
+
+/**
+ * Created by deividi.silva on 30/03/2016.
+ */
+public interface ShufflerFactory {
+}
